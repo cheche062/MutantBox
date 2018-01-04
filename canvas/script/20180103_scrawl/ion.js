f86f3726dc0ef1d62bdea4d6630f692b55326193
cheche062 <yoojunglee@126.com>
2018-01-04 12:28:02 +0800

    canvas

--- canvas/script/20180103_scrawl/ion.js
+++ canvas/script/20180103_scrawl/ion.js
@@ -1,97 +1,109 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var input =  document.getElementsByTagName('input')[0];
+var button = document.getElementsByTagName('button')[0];
 
 var img = new Image();
-// 总的坐标数组
-var dotList = [];
+
 var speed = 6;
 
 img.onload = function() {
-    init();
-    startAnimate();
+    
+    button.onclick = function() {
+        var dotList =  init(input.value);
+        startAnimate(dotList);
+    }
+
+
 }
 
-// img.src = "../../images/image.png";
-img.src = "../../images/mj.png";
+img.src = "../../images/image.png";
+// img.src = "../../images/mj.png";
 
 class Dot {
     constructor(x, y) {
         // 终点坐标
         this.target = {
-                x,
-                y
-            }
-            // 起点坐标
-        this.current = {
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height
+            x,
+            y
         }
 
+        // 起点坐标
+        this.start = {
+            x: canvas.width / 2,
+            y: canvas.height / 2
+        }
+
+        // 当前坐标
+        this.current = Object.assign({}, this.start);
         this.isDone = false;
         this.init();
     }
 
     init() {
-        this.speedX = this.target.x >= this.current.x ? speed : -speed;
-        this.speedY = this.target.y >= this.current.y ? speed : -speed;
+        this.frameNum = 0;
+        this.frameCount = Math.ceil(3000 / 16.66);
+        this.delay = Math.floor(Math.random() * 100);
+        
     }
 
     update() {
         if (this.isDone) return;
-        this.current.x += this.speedX;
-        this.current.y += this.speedY;
+        if(this.delay-- > 0) return;
+        this.frameNum++;
 
-        if (this.speedX >= 0) {
-            if (this.current.x >= this.target.x) {
-                this.current.x = this.target.x;
-            }
-        } else {
-            if (this.current.x < this.target.x) {
-                this.current.x = this.target.x;
-            }
-        }
-
-        if (this.speedY >= 0) {
-            if (this.current.y >= this.target.y) {
-                this.current.y = this.target.y;
-            }
-        } else {
-            if (this.current.y < this.target.y) {
-                this.current.y = this.target.y;
-            }
-        }
+        this.current.x = this.easeInOutCubic(this.frameNum, this.start.x, this.target.x - this.start.x, this.frameCount);
+        this.current.y = this.easeInOutCubic(this.frameNum, this.start.y, this.target.y - this.start.y, this.frameCount);
 
         if (this.current.x === this.target.x && this.current.y === this.target.y) {
             this.isDone = true;
         }
     }
+
+    // t 当前时间
+    // b 初始值
+    // c 总位移
+    // d 总时间
+    easeInOutCubic (t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+        return c / 2 * ((t -= 2) * t * t + 2) + b;
+    }
 }
 
-function init() {
+function init(text) {
+    // 总的坐标数组
+    var dotList = [];
     var _w = canvas.width;
     var _h = canvas.height;
-    var posX = (_w - img.width) / 2;
-    var posY = (_w - img.height) / 2 + 200;
-    context.drawImage(img, posX, posY);
+    var posX = (_w - img.width) / 2 - 180;
+    var posY = (_w - img.height) / 2 + 100;
+    context.clearRect(0, 0, _w, _h);
+
+    // context.drawImage(img, posX, posY);
+
+    context.font = "200px Helvetica";
+    context.fillText(text, posX, posY);
 
     var imagedata = context.getImageData(0, 0, _w, _h);
 
     for (let x = 0, w = _w; x < w; x += 6) {
         for (let y = 0, h = _h; y < h; y += 6) {
             let cur = (y * w + x) * 4;
-            if (imagedata.data[cur + 3] >= 128 && imagedata.data[cur] < 100) {
+            if (imagedata.data[cur + 3] > 0) {
                 dotList.push(new Dot(x, y));
             }
         }
     }
 
-
     context.clearRect(0, 0, _w, _h);
     // console.log(dotList)
+    return dotList;
 }
 
 
-function startAnimate() {
+function startAnimate(dotList) {
+    cancelAnimationFrame(draw);
+
     var w = canvas.width;
     var h = canvas.height;
     var total = dotList.length;
