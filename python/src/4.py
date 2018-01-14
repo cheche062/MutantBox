cee44888e8b529e3d080bffa3f975692379d9310
cheche062 <yoojunglee@126.com>
2018-01-14 22:24:42 +0800

    python

---
+++ python/src/4.py
@@ -0,0 +1,24 @@
+import math
+
+list1 = ['cheche', 'uknow', 'max']
+list2 = [18, 19, 20]
+tuple1 = ()
+
+dict1 = {
+    "name": "cheche",
+    "age": 19,
+    "job": "Python"
+}
+
+girls = ["marry", "momo", "hany", "alic"]
+letterGirls = {}
+
+def hello(name):
+    print('hello', name)
+
+hello("cheche")
+
+
+    
+    
+    
---
+++ python/src/function.py
@@ -0,0 +1,16 @@
+__metaclass__ = type
+
+
+
+class FooBar:
+    def __init__(this, value):
+        this.somevar = value
+
+f = FooBar(100)
+print(f.somevar)
+
+f.name = "fff"
+print(f.name)
+
+
+
