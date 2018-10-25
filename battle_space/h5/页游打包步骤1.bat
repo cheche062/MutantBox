937ddfde73253664b9666091d4bde7eda5b6d759
cheche062 <yoojunglee@126.com>
2018-10-25 12:22:56 +0800

    h5html

---
+++ battle_space/h5/页游打包步骤1.bat
@@ -0,0 +1,8 @@
+﻿f:
+cd F:\147
+layaair-cmd resourceVersion -i appRes -o . -n %date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%
+
+rem 压缩
+"C:\Program Files\WinRAR\WinRAR.exe" a manifest.zip manifest.json
+rem 重命名
+ren manifest.zip manifest.jpg
---
+++ battle_space/h5/页游打包步骤2.bat
@@ -0,0 +1,5 @@
+del manifest.jpg
+rem 压缩
+"C:\Program Files\WinRAR\WinRAR.exe" a manifest.zip manifest.json
+rem 重命名
+ren manifest.zip manifest.jpg
